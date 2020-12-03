4049bda665d283708d1f31f9217fee6d51fa9b2f
Felix Angell <felixangell@otzaf.org>
2020-12-03 22:43:04 +0000

    converter to use constant rather than the magical 4

--- src/main/kotlin/org/otzaf/exec/ByteConverter.kt
+++ src/main/kotlin/org/otzaf/exec/ByteConverter.kt
@@ -6,13 +6,13 @@ import java.nio.ByteOrder
 class ByteConverter {
     companion object {
         fun intToBytes(value: Int): ByteArray {
-            val bytes = ByteArray(4)
+            val bytes = ByteArray(INT_SIZE)
             ByteBuffer.wrap(bytes).order(ByteOrder.BIG_ENDIAN).putInt(value)
             return bytes
         }
 
         fun bytesToInt(data: ByteArray): Int {
-            val bytes = ByteArray(4)
+            val bytes = ByteArray(INT_SIZE)
             val buff = ByteBuffer.wrap(bytes).order(ByteOrder.BIG_ENDIAN)
             buff.put(data)
             return buff.getInt(0)
